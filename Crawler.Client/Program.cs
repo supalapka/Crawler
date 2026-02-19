0506f57b2bb88e06c7351743502c97f864b0d64a
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-02-19 17:27:11 +0700

    Crawler.Client: input filter string and publish StartCrawl via MassTransit

--- Crawler.Client/Program.cs
+++ Crawler.Client/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Crawler.Contracts;
+using MassTransit;
+
+class Program
+{
+    static async Task Main()
+    {
+        var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+        {
+            cfg.Host("localhost", "/", h =>
+            {
+                h.Username("guest");
+                h.Password("guest");
+            });
+        });
+
+        await bus.StartAsync();
+
+        string filter = "";
+
+        while (filter.ToLower() != "exit")
+        {
+
+            Console.Write("Enter filter: ");
+            filter = Console.ReadLine();
+
+            if (filter.ToLower() == "exit")
+                continue;
+
+            await bus.Publish(new StartCrawl(filter));
+
+            Console.WriteLine("Message sent");
+        }
+
+        await bus.StopAsync();
+    }
+}

397f9493f4e1210a3e218bb6c550eb4cffdf6bc1
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-02-19 17:27:26 +0700

    Crawler.ResultListener: output UrlMatched

--- Crawler.ResultListener/Program.cs
+++ Crawler.ResultListener/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Crawler.ResultListener;
+using MassTransit;
+
+class Program
+{
+    static async Task Main()
+    {
+        var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+        {
+            cfg.Host("localhost", "/", h =>
+            {
+                h.Username("guest");
+                h.Password("guest");
+            });
+
+            cfg.ReceiveEndpoint("crawler.start", e =>
+            {
+                e.Consumer<UrlMatchedConsumer>();
+            });
+        });
+
+        await bus.StartAsync();
+
+        Console.WriteLine("Listener + Output started");
+        Console.ReadLine();
+    }
+}
---
+++ Crawler.ResultListener/UrlMatchedConsumer.cs
@@ -0,0 +1,14 @@
+﻿using Crawler.Contracts;
+using MassTransit;
+
+namespace Crawler.ResultListener
+{
+    class UrlMatchedConsumer : IConsumer<UrlMatched>
+    {
+        public Task Consume(ConsumeContext<UrlMatched> context)
+        {
+                Console.WriteLine($"[Listener + output] Coin matched:{context.Message.Url}: {context.Message.Url}");
+            return Task.CompletedTask;
+        }
+    }
+}

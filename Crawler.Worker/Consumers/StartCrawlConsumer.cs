310ec2d2c365c785218ff40319b0c7132de00690
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-02-19 17:27:07 +0700

    Create StartCrawlConsumer.cs

---
+++ Crawler.Worker/Consumers/StartCrawlConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Crawler.Contracts;
+using Crawler.Worker.Services;
+using MassTransit;
+
+namespace Crawler.Worker.Consumers
+{
+    class StartCrawlConsumer : IConsumer<StartCrawl>
+    {
+        private readonly ICrawlService _crawlService;
+
+        public StartCrawlConsumer(ICrawlService crawlService)
+        {
+            _crawlService = crawlService;
+        }
+
+        public Task Consume(ConsumeContext<StartCrawl> context)
+        {
+            return _crawlService.StartAsync(
+                context.Message.Filter,
+                context.CancellationToken
+            );
+        }
+    }
+}

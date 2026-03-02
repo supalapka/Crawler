26211fbbef78df570c0fc573583a448426fa682a
Yuriy Balint <46222135+supalapka@users.noreply.github.com>
2026-03-02 12:33:25 +0700

    Refactor program.cs: creat ServiceCollectionExtensions

--- Crawler.Worker/Program.cs
+++ Crawler.Worker/Program.cs
@@ -1,63 +1,11 @@
-using Crawler.Worker.Config;
-using Crawler.Worker.Consumers;
-using Crawler.Worker.Infrastructure;
-using Crawler.Worker.Services;
-using GreenPipes;
-using MassTransit;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 
 var builder = Host.CreateDefaultBuilder(args)
     .ConfigureServices((ctx, services) =>
     {
-        services.AddOptions<ForklogCrawlPolicy>()
-            .Bind(ctx.Configuration.GetSection("ForklogCrawlPolicy"))
-            .Validate(p => p.MaxPages > 0);
-
-        services.AddOptions<RetryPolicy>()
-            .Bind(ctx.Configuration.GetSection("RetryPolicy"))
-            .Validate(p => p.RetryCount > 0 && p.IntervalSeconds > 0);
-
         services.AddMemoryCache();
-        services.AddHttpClient<PageFetcher>();
-        services.AddSingleton<ICrawlService, ForkLogCrawlService>();
-
-        services.AddMassTransit(x =>
-        {
-            x.AddConsumer<StartCrawlConsumer>();
-
-            x.UsingRabbitMq((context, cfg) =>
-            {
-                var rabbitHost = ctx.Configuration["RABBITMQ_HOST"] ?? "localhost";
-
-                cfg.Host(rabbitHost, "/", h =>
-                {
-                    h.Username("guest");
-                    h.Password("guest");
-                });
-
-                var retryPolicy = context.GetRequiredService<IOptions<RetryPolicy>>().Value;
-
-                cfg.ReceiveEndpoint("crawler.start", e =>
-                {
-                    e.ConcurrentMessageLimit = 1;
-                    e.PrefetchCount = 1;
-                    e.UseMessageRetry(r =>
-                        r.Intervals(
-                            Enumerable.Repeat(
-                                TimeSpan.FromSeconds(retryPolicy.IntervalSeconds),
-                                retryPolicy.RetryCount
-                            ).ToArray()
-                        )
-                    );
-
-                    e.ConfigureConsumer<StartCrawlConsumer>(context);
-                });
-            });
-        });
-
-        services.AddHostedService<MassTransitHostedService>();
+        services.AddCrawlerServices(ctx.Configuration);
+        services.AddCrawlerMassTransit(ctx.Configuration);
     });
 
 await builder.Build().RunAsync();
---
+++ Crawler.Worker/ServiceCollectionExtensions.cs
@@ -0,0 +1,69 @@
+using Crawler.Worker.Config;
+using Crawler.Worker.Consumers;
+using Crawler.Worker.Infrastructure;
+using Crawler.Worker.Services;
+using GreenPipes;
+using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+internal static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCrawlerServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<ForklogCrawlPolicy>()
+            .Bind(configuration.GetSection("ForklogCrawlPolicy"))
+            .Validate(p => p.MaxPages > 0);
+
+        services.AddOptions<RetryPolicy>()
+            .Bind(configuration.GetSection("RetryPolicy"))
+            .Validate(p => p.RetryCount > 0 && p.IntervalSeconds > 0);
+
+        services.AddHttpClient<PageFetcher>();
+        services.AddSingleton<ICrawlService, ForkLogCrawlService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddCrawlerMassTransit(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumer<StartCrawlConsumer>();
+
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                var rabbitHost = configuration["RABBITMQ_HOST"] ?? "localhost";
+
+                cfg.Host(rabbitHost, "/", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+                var retryPolicy = context.GetRequiredService<IOptions<RetryPolicy>>().Value;
+
+                cfg.ReceiveEndpoint("crawler.start", e =>
+                {
+                    e.ConcurrentMessageLimit = 1;
+                    e.PrefetchCount = 1;
+                    e.UseMessageRetry(r =>
+                        r.Intervals(
+                            Enumerable.Repeat(
+                                TimeSpan.FromSeconds(retryPolicy.IntervalSeconds),
+                                retryPolicy.RetryCount
+                            ).ToArray()
+                        )
+                    );
+
+                    e.ConfigureConsumer<StartCrawlConsumer>(context);
+                });
+            });
+        });
+
+        services.AddHostedService<MassTransitHostedService>();
+
+        return services;
+    }
+}
